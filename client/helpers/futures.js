a602104cbef63e747e3e56ab36960d02498d664a
Paweł Burda <pburda@gmail.com>
2014-10-31 02:21:41 +0100

    Settlement countdown

--- client/helpers/futures.js
+++ client/helpers/futures.js
@@ -41,5 +41,41 @@ Template.futures.helpers({
                 data: data
             }]
         };
+    },
+    settlementCountdown: function() {
+        SysSecond = 66503;
+        InterValObj = window.setInterval(SetRemainTime, 1000);
+
+        function SetRemainTime() { 
+            if (SysSecond > 0) { 
+                SysSecond = SysSecond - 1; 
+                var second = Math.floor(SysSecond % 60);
+                var minute = Math.floor((SysSecond / 60) % 60);
+                var hour = Math.floor((SysSecond / 3600) % 24);
+                var day = Math.floor((SysSecond / 3600) / 24);
+                var html = 'Settlement Countdown: ';
+                if (day > 0){
+                    html += '<strong>'+day+'</strong>D ';
+                }
+                if (hour > 0){
+                    html += '<strong>'+hour+'</strong>';
+                }
+                if (minute > 0){
+                    html += '<strong>:'+minute+'</strong>';
+                }
+                if (second > 0){
+                    html += '<strong>:'+second+'</strong>';
+                }
+                else {
+                    html += '<strong>:0</strong>';
+                }
+
+                $('#settlement-countdown').html(html);
+            } 
+            else {
+                window.clearInterval(InterValObj); 
+                $('#settlement-countdown').html('Settlement'); 
+            } 
+        }
     }
 });
